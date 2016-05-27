79488255f5e30fb2e5499fac04c2778951ce455e
Swair Shah <swairshah@gmail.com>
2016-05-27 13:54:25 -0500

    fix static control decisions

--- rulebased_network/control_static_baseline.m
+++ rulebased_network/control_static_baseline.m
@@ -4,5 +4,5 @@ function [] = control_static_baseline(r1, r2, r3, r4, r5, r6)
     r3.fwd_rules = [0 1; 1 1];
     r4.fwd_rules = [0 0; 1 1];
     r5.fwd_rules = [0 1; 0 1];
-    r6.fwd_rules = [0 0; 0 1];
+    r6.fwd_rules = [0 1; 0 1];
 end
